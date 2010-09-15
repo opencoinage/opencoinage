c5bec037542770aa1337b0d8e308e48dc6db9a70
Arto Bendiken <arto@bendiken.net>
2010-09-15 17:18:34 +0200

    Imported stubs for the JavaScript and PHP client libraries.

---
+++ src/js/opencoinage.js
@@ -0,0 +1,6 @@
+/**
+ * OpenCoinage.org XML-RPC API client for JavaScript.
+ *
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/>
+ */
---
+++ src/php/opencoinage.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * OpenCoinage.org XML-RPC API client for PHP 5.3+.
+ *
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/>
+ */
