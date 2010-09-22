17b014c5f10765bc66f339cbc5ec17d69f605d65
Arto Bendiken <arto@bendiken.net>
2010-09-22 17:37:43 +0200

    Added an OpenCoinage::Token#id accessor.

--- lib/opencoinage/token.rb
+++ lib/opencoinage/token.rb
@@ -2,6 +2,19 @@ module OpenCoinage
   ##
   # A digital currency token.
   class Token
-    # TODO
+    ##
+    # Initializes a new token.
+    #
+    # @param  [Integer, #to_i] id
+    #   a unique identifier
+    def initialize(id)
+      @id = id.to_i
+    end
+
+    ##
+    # A token-specific unique identifier of arbitrary length.
+    #
+    # @return [Integer]
+    attr_reader :id
   end # Token
 end # OpenCoinage
