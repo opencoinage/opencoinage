efdb12982bad740c0d45b6f7ee303aba22ab2346
Arto Bendiken <arto@bendiken.net>
2010-09-23 03:11:13 +0200

    Added an OpenCoinage::Currency#issuer accessor.

--- lib/opencoinage/currency.rb
+++ lib/opencoinage/currency.rb
@@ -2,6 +2,19 @@ module OpenCoinage
   ##
   # A digital currency contract.
   class Currency
-    # TODO
+    ##
+    # Initializes the currency.
+    #
+    # @param  [Hash{Symbol => Object}] options
+    # @option options [Issuer] :issuer (nil)
+    def initialize(options = {})
+      @issuer = options[:issuer]
+    end
+
+    ##
+    # The issuer of this currency.
+    #
+    # @return [Issuer]
+    attr_reader :issuer
   end # Currency
 end # OpenCoinage
