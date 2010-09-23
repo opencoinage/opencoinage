008786ea863767627b281b522d2382ffd92a60b9
Arto Bendiken <arto@bendiken.net>
2010-09-23 03:23:13 +0200

    Added an OpenCoinage::Issuer#uri accessor.

--- lib/opencoinage/issuer.rb
+++ lib/opencoinage/issuer.rb
@@ -4,6 +4,20 @@ module OpenCoinage
   class Issuer
     RDF_TYPE = Vocabulary[:Issuer]
 
-    # TODO
+    ##
+    # Initializes the issuer.
+    #
+    # @param  [RDF::URI, String, #to_s] uri
+    # @param  [Hash{Symbol => Object}]  options
+    def initialize(uri = nil, options = {})
+      @uri = RDF::URI(uri || UUID.generate(:urn))
+    end
+
+    ##
+    # The URI of this issuer.
+    #
+    # @return [RDF::URI]
+    attr_reader  :uri
+    alias_method :to_uri, :uri
   end # Issuer
 end # OpenCoinage
