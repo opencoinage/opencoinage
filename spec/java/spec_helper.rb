ba721d86b357466c37c008b9a05100182693d939
Arto Bendiken <arto@bendiken.net>
2010-10-16 15:43:47 +0200

    Ensured that Buildr is able to build the Java SDK even if the .jar file doesn't exist.

--- spec/java/spec_helper.rb
+++ spec/java/spec_helper.rb
@@ -2,4 +2,8 @@ require 'opencoinage'
 include OpenCoinage
 
 require 'java' # @see http://jruby.org/
-require File.join(File.dirname(__FILE__), '..', '..', 'pkg', 'java', "opencoinage-#{OpenCoinage::VERSION}.jar")
+begin
+  require File.join(File.dirname(__FILE__), '..', '..', 'pkg', 'java', "opencoinage-#{OpenCoinage::VERSION}.jar")
+rescue LoadError => e
+  warn e
+end
