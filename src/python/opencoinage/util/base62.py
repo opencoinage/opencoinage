1543a5975b3c36b907380079b738baab16565483
Arto Bendiken <arto@bendiken.net>
2010-10-17 10:07:00 +0200

    Implemented Base62 encoding/decoding for the Python SDK.

--- src/python/opencoinage/util/base62.py
+++ src/python/opencoinage/util/base62.py
@@ -12,10 +12,22 @@ class Base62(object):
   def encode(number):
     """Encodes a number using Base62 encoding."""
     number = int(number)
-    pass # TODO
+    result = []
+    while number > 0:
+      number, digit = divmod(number, BASE)
+      result.insert(0, DIGITS[digit])
+    if len(result) == 0:
+      return DIGITS[0]
+    else:
+      return ''.join(result)
 
   @staticmethod
   def decode(string):
     """Decodes a string using Base62 encoding."""
     string = str(string)
-    pass # TODO
+    result = 0
+    index  = 0
+    for char in reversed(string):
+      result += DIGITS.index(char) * (BASE ** index)
+      index  += 1
+    return result
