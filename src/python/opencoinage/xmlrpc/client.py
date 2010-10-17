f321a0b350cabcd524137b2b31cc9c9247efc931
Arto Bendiken <arto@bendiken.net>
2010-10-17 21:27:52 +0200

    Implemented the XML-RPC client for the Python SDK.

--- src/python/opencoinage/xmlrpc/client.py
+++ src/python/opencoinage/xmlrpc/client.py
@@ -7,20 +7,22 @@ class Client(object):
   url = None
 
   def __init__(self, url, **kwargs):
-    self.url = url
+    # TODO: support XMPP as a transport (XEP-0009, aka XMPP-RPC).
+    self.url    = url
+    self.server = xmlrpclib.ServerProxy(self.url)
 
   def version(self):
     """Returns the server's version number."""
-    pass # TODO
+    return self.server.opencoinage.version()
 
   def verify(self, token):
     """Returns `True` if the given `token` is valid, `False` otherwise."""
-    pass # TODO
+    return bool(self.server.opencoinage.verify(str(token))) # FIXME
 
   def describe(self, token):
     """Returns information about the given `token`."""
-    pass # TODO
+    return self.server.opencoinage.describe(str(token))
 
   def reissue(self, token, identifier = None):
     """Issues a new token equivalent to the given `token`."""
-    pass # TODO
+    return self.server.opencoinage.reissue(str(token)) # FIXME
